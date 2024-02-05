f4882ef66a22e2cf575325de0c70bfafabb1dee2
UptGiant <105814982+UptGiant@users.noreply.github.com>
2024-02-04 20:46:44 -0600

    Prompt user location
    Prompt to allow access to get user location.

---
+++ hooks/useGetLocation.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import * as Location from 'expo-location'
+
+export const useGetLocation = () => {
+    const [location, setLocation] = useState(null)
+    const [lat, setLat] = useState<number | null>(null)
+    const [lon, setLon] = useState<number | null>(null)
+
+    useEffect(() => {
+        (async() => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.log('Permission Denied')
+                return;
+            }
+            let CurrentLocation:any = await Location.getCurrentPositionAsync({});
+            setLocation(CurrentLocation)
+            setLat(CurrentLocation.coords.latitude)
+            setLon(CurrentLocation.coords.longitude)
+            console.log(CurrentLocation)
+        })();
+    }, []);
+    return [lat, lon]
+}
