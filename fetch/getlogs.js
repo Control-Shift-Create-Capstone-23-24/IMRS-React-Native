5e9601b2f0b4ac2ebfac674a1018f3a220009abc
Brian121301 <briannajera3@gmail.com>
2024-04-14 19:35:32 -0500

    Update getlogs.js

--- fetch/getlogs.js
+++ fetch/getlogs.js
@@ -1,23 +1,53 @@
+
+// const WebSocket = require('ws');
+
+// const WebSocketEndpoint = 'wss://w1nthfyrli.execute-api.us-west-2.amazonaws.com/production/';
+
+// const webSocket = new WebSocket(WebSocketEndpoint);
+
+// webSocket.onopen = function(event) {
+//     console.log('WebSocket connection established');
+// };
+
+// webSocket.onmessage = function(event) {
+//     const message = event.data;
+//     console.log('Message received:', message);
+// };
+
+// webSocket.onclose = function(event) {
+//     console.log('WebSocket connection closed');
+// };
+
+// webSocket.onerror = function(event) {
+//     console.error('WebSocket error:', event);
+// };
+
 const WebSocket = require('ws');
 
-const WebSocketEndpoint = 'wss://w1nthfyrli.execute-api.us-west-2.amazonaws.com/production/';
+// Define the WebSocket endpoint URL
+const webSocketEndpoint = 'wss://3rkw9ldyf6.execute-api.us-west-2.amazonaws.com/Inprogress/';
 
-const webSocket = new WebSocket(WebSocketEndpoint);
+// Create a WebSocket client
+const webSocket = new WebSocket(webSocketEndpoint);
 
-webSocket.onopen = function(event) {
+// Handle successful connection
+webSocket.on('open', function open() {
     console.log('WebSocket connection established');
-};
+});
 
-webSocket.onmessage = function(event) {
-    const message = event.data;
-    console.log('Message received:', message);
-};
+// Handle messages from the server
+webSocket.on('message', function incoming(data) {
+    console.log('Received new item:', data);
+});
 
-webSocket.onclose = function(event) {
+// Handle WebSocket connection errors
+webSocket.on('error', function error(err) {
+    console.error('WebSocket error:', err);
+});
+
+// Handle WebSocket connection closing
+webSocket.on('close', function close() {
     console.log('WebSocket connection closed');
-};
+});
 
-webSocket.onerror = function(event) {
-    console.error('WebSocket error:', event);
-};
 
