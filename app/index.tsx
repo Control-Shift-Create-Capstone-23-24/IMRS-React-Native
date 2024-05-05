9f41d79767b96567ca5bca74dc4e480a51581fdb
BE-1472 <bryce.ellinger1@gmail.com>
2024-05-04 19:48:53 -0500

    merged main and sudo-main

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,13 @@ import { Redirect } from "expo-router";
 import { Platform } from "react-native";
 
 export default function Index() {
+    /*
     // Web version redirect to here
     if (Platform.OS === 'web') {
         console.log('Web browser detected. Redirecting to WebLandingPage.')
         return <Redirect href="/webLandingPage"/>
     }
     // IOS and Android version redirect to here
+    */
     return <Redirect href="/landingPage" />
 }
